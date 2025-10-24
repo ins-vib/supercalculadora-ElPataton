f3fb45ae8140567569b3efad1bb4656b51c52bfd
ElPataton <153860657+ElPataton@users.noreply.github.com>
2025-10-24 12:28:22 +0200

    second commit

--- src/main/java/org/yourcompany/calculadora/Calculadora.java
+++ src/main/java/org/yourcompany/calculadora/Calculadora.java
@@ -1,7 +1,7 @@
 
 
 package org.yourcompany.calculadora;
-
+import java.util.Random;
 /**
  *
  * @author Posa el teu nom
@@ -37,9 +37,18 @@ public class Calculadora {
         // System.out.println("2 elevat a la potència 3: " + calcularPotencia(2, 3));
         // System.out.println("5 elevat a la potència 4: " + calcularPotencia(5, 4));
         // System.out.println("3 elevat a la potència 5: " + calcularPotencia(3, 5));
-
-       
         
+        // Multiple moneda
+        // System.out.println("Numero de cares al tirar 50 monedes " + numcares(50));
+        // System.out.println("Numero de cares al tirar 100 monedes " + numcares(100));
+        // System.out.println("Numero de cares al tirar 1000 monedes " + numcares(1000));
+
+        //Cinema
+        // System.out.println("Si vas al cine amb preu base 5€ al cap de setmana amb carnet jove " + Cinema(5, true, true) + " €");
+        // System.out.println("Si vas al cine amb preu base 15€ entre setmana amb carnet jove " + Cinema(15, false, true) + " €");
+        // System.out.println("Si vas al cine amb preu base 55€ al cap de setmana sense carnet jove " + Cinema(55, true, false) + " €");
+    
+    
     }
 
     /**
@@ -50,6 +59,7 @@ public class Calculadora {
      * @param nombre Número del qual es vol saber el nombre de dígits
      * @return Quantitat de dígits que té {@code nombre}
      */
+    
     public static int nombreDigits(int nombre) {
         if (nombre == 0) {
             return 1; // el 0 té un dígit
@@ -62,5 +72,38 @@ public class Calculadora {
         return comptador;
     }
 
+    public static int numcares(int vegades) {
+        int randomnum = 0;
+        int cara = 0;
+        Random generator = new Random();
+        for (int i = 0; i < vegades ; i++){
+            randomnum = generator.nextInt(0,2);
+            if (randomnum == 0){
+                cara++;
+            }
+        }
+        return cara;
+        
+    }
+
+    public static int sumaPrimersNumeros(int n) { 
+        int suma = n;
+        for (int i = 0; i <= n; i++) {
+            suma = suma + i;
+        }
+        return suma;
+    }
+
+    public static double Cinema(double preu, boolean capdesetmana, boolean carnetjove) {        
+        if(capdesetmana == true) {
+            preu = preu + (preu * 0.10);
+        }
+       if (carnetjove == true){
+            preu = preu - (preu * 0.15);
+        }
+        return preu;
+    }    
+
 
 }
+
