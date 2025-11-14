cc59beef1533e69090527e5d9c9d6088690653c6
ElPataton <153860657+ElPataton@users.noreply.github.com>
2025-11-14 10:19:55 +0100

    Third commit

--- src/main/java/org/yourcompany/calculadora/Calculadora.java
+++ src/main/java/org/yourcompany/calculadora/Calculadora.java
@@ -2,6 +2,7 @@
 
 package org.yourcompany.calculadora;
 import java.util.Random;
+import java.util.Scanner;
 /**
  *
  * @author Posa el teu nom
@@ -11,12 +12,12 @@ import java.util.Random;
 public class Calculadora {
 
     public static void main(String[] args) {
-
+        mostrarMenuConsola();
         // Exemples de crida per al mètode nombreDigits
-        System.out.println("Nombre de dígits de 1234: " + nombreDigits(1234));
-        System.out.println("Nombre de dígits de 56789: " + nombreDigits(56789));
-        System.out.println("Nombre de dígits de -101010: " + nombreDigits(-101010));
-        System.out.println("Nombre de dígits de 0: " + nombreDigits(0));
+        // System.out.println("Nombre de dígits de 1234: " + nombreDigits(1234));
+        // System.out.println("Nombre de dígits de 56789: " + nombreDigits(56789));
+        // System.out.println("Nombre de dígits de -101010: " + nombreDigits(-101010));
+        // System.out.println("Nombre de dígits de 0: " + nombreDigits(0));
 
         // Exemples de crida per al mètode sumaPrimersNumeros
         // System.out.println("Suma dels primers 5 números: " + sumaPrimersNumeros(5));
@@ -60,49 +61,165 @@ public class Calculadora {
      * @return Quantitat de dígits que té {@code nombre}
      */
     
-    public static int nombreDigits(int nombre) {
-        if (nombre == 0) {
-            return 1; // el 0 té un dígit
-        }        
-        int comptador = 0;
-        while (nombre != 0) {
-            nombre /= 10;
-            comptador++;
-        }
-        return comptador;
-    }
-
-    public static int numcares(int vegades) {
-        int randomnum = 0;
-        int cara = 0;
-        Random generator = new Random();
-        for (int i = 0; i < vegades ; i++){
-            randomnum = generator.nextInt(0,2);
-            if (randomnum == 0){
-                cara++;
+    public static void mostrarMenuConsola(){
+        Scanner lectura = new Scanner(System.in);
+        int opcio = 1;
+        while (opcio != 0) {
+            System.out.println("Supercalculadora!");
+            System.out.println("1: Suma dels primers numeros");
+            System.out.println("2: CalcularFactorial");
+            System.out.println("3: Suma de Quadrats");
+            System.out.println("4: Calcul de potencies");
+            System.out.println("5: Nombre de digits");
+            System.out.println("6: Cara o creu");
+            System.out.println("7: Entrades cinema");
+            System.out.println("0: Sortir");
+            System.out.println("Quina opcio de la calculadora vols utilitzar?");
+            opcio = lectura.nextInt();
+            switch (opcio) {
+                case 0 -> {
+                    opcio=0;
+                    return;
+                }
+                case 1 -> {
+                    int n = 0;
+                    System.out.println("Fins a quin numero vols sumar?");
+                    n = lectura.nextInt();
+                    System.out.println(sumaPrimersNumeros(n));
+                }
+                case 2 -> {
+                    int n = 0;
+                    System.out.println("De quin numero vols fer el factorial?");
+                    n = lectura.nextInt();
+                    System.out.println(calcularFactorial(n));
+                }
+                case 3 ->{
+                    int n = 0;
+                    System.out.println("Fins a quin numero vols sumar els seus cuadrats?");
+                    n = lectura.nextInt();
+                    System.out.println(sumaQuadrats(n));
+                }
+                case 4 ->{
+                    int n = 0;
+                    int z = 0;
+                    System.out.println("Quin numero es la base de la potencia?");
+                    n = lectura.nextInt();
+                    System.out.println("Quin numero es el exponent de la potencia?");
+                    z = lectura.nextInt();
+                    System.out.println(calcularPotencia(n,z));
+                }
+                case 5 ->{
+                    int n = 0;
+                    System.out.println("Digues un numero per calcular els digits");
+                    n = lectura.nextInt();
+                    System.out.println(nombreDigits(n));
+                }
+                case 6 ->{
+                    int n = 0;
+                    System.out.println("Cuantes monedas vols llançar?");
+                    n = lectura.nextInt();
+                    System.out.println(numcares(n));
+                }
+                case 7 ->{
+                    double n = 5;
+                    boolean z = false;
+                    boolean x = false;
+                    char capdesetmana;
+                    char carnetJove;
+                    System.out.println("Es cap de setmana? S/N");
+                    capdesetmana = lectura.next().charAt(0);
+                    System.out.println("Tens carnet jove? S/N");
+                    carnetJove = lectura.next().charAt(0);
+                    if ('S' == capdesetmana){
+                        z = true;
+                    }
+                    else{
+                        z = false;
+                    }
+                    if ('S' == carnetJove){
+                        x = true;
+                    }
+                    else{
+                        x = false;
+                    }
+                    System.out.println("La entrada costa " + Cinema(n, z, x) + " €");
+                }
+                default -> {
+                    System.out.println("");
+                }
             }
         }
-        return cara;
-        
     }
+
+
+public static int nombreDigits(int nombre) {
+    if (nombre == 0) {
+        return 1; // el 0 té un dígit
+    }        
+    int comptador = 0;
+    while (nombre != 0) {
+        nombre /= 10;
+        comptador++;
+    }
+    return comptador;
+}
 
     public static int sumaPrimersNumeros(int n) { 
-        int suma = n;
-        for (int i = 0; i <= n; i++) {
-            suma = suma + i;
-        }
-        return suma;
+    int suma = 0;
+    for (int i = 0; i <= n; i++) {
+        suma = suma + i;
+    }
+    return suma;
+}
+
+public static int calcularFactorial(int n){
+    int factorial = 1;
+    for (int i = 1; i <=n; i++){
+        factorial = factorial * i;
+    }
+    return factorial;
+}
+
+public static int sumaQuadrats(int n){
+    int quadrat = 0;
+    for(int i = 0; i <=n; i++){
+        quadrat = quadrat + (i*i);
     }
 
-    public static double Cinema(double preu, boolean capdesetmana, boolean carnetjove) {        
-        if(capdesetmana == true) {
-            preu = preu + (preu * 0.10);
+    return quadrat;
+}
+public static int calcularPotencia(int base, int exponent){
+    int resultat = base;
+    for (int i=1;i < exponent; i++){
+        resultat *=  base ;
+    }
+
+    return resultat;
+}
+
+public static int numcares(int vegades) {
+    int randomnum = 0;
+    int cara = 0;
+    Random generator = new Random();
+    for (int i = 0; i < vegades ; i++){
+        randomnum = generator.nextInt(0,2);
+        if (randomnum == 0){
+            cara++;
         }
-       if (carnetjove == true){
-            preu = preu - (preu * 0.15);
-        }
-        return preu;
-    }    
+    }
+    return cara;
+    
+} 
+
+public static double Cinema(double preu, boolean capdesetmana, boolean carnetjove) {        
+    if(capdesetmana == true) {
+        preu = preu + (preu * 0.10);
+    }
+    if (carnetjove == true){
+        preu = preu - (preu * 0.15);
+    }
+    return preu;
+}    
 
 
 }
